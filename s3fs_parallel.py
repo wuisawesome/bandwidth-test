df7ca94e6b40903e7ee5949c91b9588a7834da93
Alex Wu <itswu.alex@gmail.com>
2020-02-18 16:32:06 -0800

    Some stuff

---
+++ s3fs_parallel.py
@@ -0,0 +1,22 @@
+import ray
+import s3fs
+
+ray.init()
+fs = s3fs.S3FileSystem(anon=False)
+
+files = fs.ls("anyscale-data/bandwidth-benchmark")[:1000]
+
+@ray.remote
+def download(path):
+    data = fs.open(path, 'rb').read()
+    x = 0
+    for byte in data:
+        x ^= byte
+
+
+start = clock()
+ids = [download.remote(path) for path in files]
+ray.get(ids)
+end = clock()
+
+print("Total download time: ", (end - start))
